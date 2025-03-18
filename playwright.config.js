1f40d658cf7988e426423a846064f64c0812e4ce
L1ghtSta4 <10252158@stud.op.edu.ua>
2025-03-18 20:09:49 -0100

    third try

--- playwright.config.js
+++ playwright.config.js
@@ -34,6 +34,6 @@ export default defineConfig({
     webServer: {
         command: 'npm run start',
         url: 'http://localhost:3000',
-        reuseExistingServer: !process.env.CI,
+        reuseExistingServer: true, // Добавь эту строку
     },
 });
